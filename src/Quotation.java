d7af0631db2dadcdd6335efc0913a564aa64af7f
Virusim <50884391+VirusIm@users.noreply.github.com>
2022-01-31 07:20:21 +0000

    New commit

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "Be yourself; everyone else is already taken.";
+  String author = "Oscar Wilde";
+  public void display() {
+    System.out.println(quote);
+    System.out.println(author);
+  }
+}
